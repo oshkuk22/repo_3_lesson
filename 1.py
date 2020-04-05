dea198c8d0bda1f22ff51c88401a00e4d9bdccdd
oshkuk2277 <oshkuk@yandex.ru>
2020-04-05 17:15:01 +0300

    First task is executed (1.py)

---
+++ 1.py
@@ -0,0 +1,32 @@
+# 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и
+# выполняющую их деление. Числа запрашивать у пользователя, предусмотреть
+# обработку ситуации деления на ноль.
+# -*- coding: utf-8 -*-
+
+flag = True
+
+
+def division_func(dividend, divider):
+    try:
+        return print(f'Rezult of the division {dividend}/{divider} = {(dividend / divider):.3f}')
+    except ZeroDivisionError:
+        return print("Error. Division by zero")
+
+
+while flag:
+    try:
+        one_number = float(input('Input the first number:'))
+        flag = False
+    except ValueError:
+        print('Entered value is not a number!!!')
+
+flag = True
+
+while flag:
+    try:
+        two_number = float(input('Input the second number:'))
+        flag = False
+    except ValueError:
+        print('Entered value is not a number!!!')
+
+division_func(one_number, two_number)
